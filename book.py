bb724ecc1218a5b3f309b96ba83267091a8fdecd
watermelon <76525576+watermelon1024@users.noreply.github.com>
2023-06-21 14:46:55 +0800

    Update book.py

--- book.py
+++ book.py
@@ -1,13 +1,26 @@
 import io
+import re
 
 import requests
 
 from bs4 import BeautifulSoup
 
 
-class crawler():
-    def __init__(self, link: str = None) -> None:
-        soup = self.get_html(link)
+chinese_char = re.compile(r"[\u4e00-\u9fa5]")
+
+def get_html(link: str) -> BeautifulSoup | None:
+    try:
+        # 發送 GET 請求取得網頁內容
+        response = requests.get(link)
+        # 使用 BeautifulSoup 解析 HTML
+        soup = BeautifulSoup(response.text, "html.parser")
+        return soup
+    except Exception:
+        return None
+
+class Czbooks():
+    def __init__(self, link: str) -> None:
+        soup = get_html(link)
         detail_div = soup.find("div", class_="novel-detail")
 
         self.link = link
@@ -29,21 +42,10 @@ class crawler():
     def hashtag(self):
         return ", ".join(tag.text for tag in self.hashtags)
 
-    def get_html(self, link: str) -> BeautifulSoup | None:
-        try:
-            # 發送 GET 請求取得網頁內容
-            response = requests.get(link)
-            if response.status_code != 200:
-                return None
-            # 使用 BeautifulSoup 解析 HTML
-            soup = BeautifulSoup(response.text, "html.parser")
-            return soup
-        except Exception:
-            return None
 
     def get_content(self):
-        self.content = io.StringIO()
-        self.content.writelines(f"書本連結: {self.link}\n標籤: {self.hashtag()}")
+        self.content = ""
+        self.content += f"書本連結: {self.link}\n標籤: {self.hashtag()}"
         # 逐章爬取內容
         for index, link in enumerate(self.ch_list, start=1):
             process = round(index*100/self.ch_count, 2)
@@ -51,20 +53,21 @@ class crawler():
                 f"\r進度: {index}/{self.ch_count} {process}%",
                 end=""
             )
-            soup = self.get_html("https:"+link["href"])
+            soup = get_html("https:"+link["href"])
             # 尋找章節名稱
             # 尋找章節名稱 div 標籤
             ch_name = soup.find("div", class_="name")
             # 尋找內文 div 標籤
             div_content = ch_name.find_next("div", class_="content")
             # 儲存找到的內容
-            self.content.writelines(
-                f"\n\n{'='*32} {ch_name.text} {'='*32}\n\n")
-            self.content.writelines(div_content.text.strip())
+            self.content += f"\n\n{'='*32} {ch_name.text} {'='*32}\n\n"
+            self.content += div_content.text.strip()
+        self.words_count = len(re.findall(chinese_char, self.content))
 
 
 book_url = input("請輸入網站連結: ")
-book = crawler(book_url)
+book = Czbooks(book_url)
 book.get_content()
+print(book.words_count)
 with open(f"{book.title}.txt", "w", encoding="utf-8") as file:
     file.write(book.content.getvalue())
