ea15ddb3b51a7b006f3bbc96e6e9a22635ee9a13
watermelon1024 <st102060@chps.tp.edu.tw>
2023-08-27 10:48:21 +0800

    feat: add comments

--- utils/czbook/comment.py
+++ utils/czbook/comment.py
@@ -1,3 +1,10 @@
+import aiohttp
+
+from discord import Embed
+
+from .czbook import Czbook
+
+
 class Comment:
     def __init__(
         self,
@@ -21,3 +28,47 @@ class Comment:
             "reply_to": self.reply_to,
             "date": self.timestamp,
         }
+
+
+def comments_embed(book: Czbook) -> Embed:
+    embed = Embed(
+        title=f"{book.title}評論列表",
+        url=f"https://czbooks.net/n/{book.code}",
+        color=book.get_theme_color(),
+    )
+    for comment in book.comments:
+        embed.add_field(
+            name=comment.author,
+            value=f"```{comment.message}```",
+            inline=False,
+        )
+        if len(embed) > 6000:
+            embed.remove_field(-1)
+            break
+
+    return embed
+
+
+async def update_comments(code: str) -> list[Comment]:
+    comments: list[Comment] = []
+    page = 1
+    async with aiohttp.ClientSession() as session:
+        while True:
+            async with session.get(
+                f"https://api.czbooks.net/web/comment/list?novelId={code}&page={page}&cleanCache=true"  # noqa
+            ) as response:
+                data = await response.json()
+                items = data["data"]["items"]
+            comments += [
+                Comment(
+                    comment["nickname"],
+                    comment["message"],
+                    comment["date"],
+                )
+                for comment in items
+            ]
+
+            if not (page := data.get("next")):
+                break
+
+    return comments
--- utils/czbook/czbook.py
+++ utils/czbook/czbook.py
@@ -1,5 +1,8 @@
-from .comment import Comment
+from discord import Embed
+
+from .comment import Comment, update_comments, comments_embed
 from .http import HyperLink
+from .timestamp import now_timestamp
 
 
 class Czbook:
@@ -38,3 +41,24 @@ class Czbook:
         self.chapter_list = chapter_list
         self.comments = comments
         self.last_fetch_time = last_fetch_time
+
+        self._overview_embed_cache: Embed = None
+        self._chapter_embed_cache: Embed = None
+        self._comments_embed_cache: Embed = None
+        self._comment_last_update: float = None
+
+    async def comments_embed(self, update_when_out_of_date: bool = True):
+        if (
+            update_when_out_of_date
+            and ((now := now_timestamp()) - self._comment_last_update) > 600
+        ):
+            self._comment_last_update = now
+            await self.update_comments()
+            self._comments_embed_cache = comments_embed(self)
+        elif not self._comments_embed_cache:
+            self._comments_embed_cache = comments_embed(self)
+
+        return self._comments_embed_cache
+
+    async def update_comments(self):
+        self.comments = await update_comments(self.code)
