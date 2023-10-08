f33bcb1e83a1e3ac8ab16b2d2614304e3faf4c5a
watermelon1024 <watermelonman1024@gmail.com>
2023-10-08 19:18:17 +0800

    feat: add ChapterList class

---
+++ czbook/chapter.py
@@ -0,0 +1,16 @@
+class ChapterInfo:
+    def __init__(self, name: str, url: str, content: str = None) -> None:
+        self.name = name
+        self.url = url
+        self.content = content
+        self._maybe_not_content: bool = None
+
+    def maybe_not_conetent(self):
+        if self._maybe_not_content is None:
+            self._maybe_not_content = len(self.content) < 1024
+        return self._maybe_not_content
+
+
+class ChapterList(list):
+    def __init__(self, chapter_list: list[ChapterInfo] = []) -> None:
+        super().__init__(chapter_list)
