714626ce87a05eb85d5786d7bc17ce18d921ebe2
watermelon1024 <watermelonman1024@gmail.com>
2023-10-09 23:26:20 +0800

    ref: czbook module __init__.py

--- czbook/__init__.py
+++ czbook/__init__.py
@@ -11,9 +11,12 @@ Made by @watermelon1024
 
 # flake8: noqa: F401
 from . import utils
-from .comment import CommentList, Comment
+from .novel_info import NovelInfo, Author, Category, Thumbnail
+from .novel_info.hashtag import Hashtag, HashtagList
+from .chapter import ChapterInfo, ChapterList
+from .comment import Comment, CommentList
 from .czbook import Novel, fetch_novel
 from .error import NotFoundError
-from .get_content import GetContentState
+from .get_content import GetContentState, GetContent
 from .http import HyperLink
 from .search import SearchResult, search, search_advance
