9f5e18c3f9af25872daf29708da65b8d0a604764
watermelon1024 <watermelonman1024@gmail.com>
2023-11-23 19:41:58 +0800

    feat: utils
    Discord-related utilities

--- utils/discord.py
+++ utils/discord.py
@@ -1,9 +1,37 @@
+"""
+Discord-related utilities functions for the bot.
+"""
+
 import discord
 
 
+def get_user_name(user: discord.User) -> str:
+    """
+    Get the name of a user.
+
+    :param user: The user to get the name of.
+    :type user: discord.User
+
+    :return: The name of the user.
+    :rtype: str
+    """
+    if user.discriminator == "0":
+        return f"@{user.name}"
+    return f"{user.name}#{user.discriminator}"
+
+
 async def get_or_fetch_message_from_reference(
-    message: discord.Message,
+    message: discord.Message | discord.MessageReference,
 ) -> discord.Message:
+    """
+    Get the message from a referenced message.
+
+    :param message: The message referenced to get the message from.
+    :type message: Union[discord.Message, discord.MessageReference]
+
+    :return: The message.
+    :rtype: discord.Message
+    """
     return message.reference.cached_message or await message.channel.fetch_message(
         message.reference.message_id
     )
